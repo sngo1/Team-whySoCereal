f3875b419cdc2b500053acaebb1a07f6a90141f3
Adam Abbas <aabbas@stuy.edu>
2018-04-12 13:15:38 -0400

    Cereal objects! Clicking interaction stuff

--- static/bubblractions.js
+++ static/bubblractions.js
@@ -1,9 +1,48 @@
-
-
 var objs = //function to get the array of objects
+var esveegee = document.getElementById("vimage");
+
+var make_cereal = function(n, b, p, c, f, s, sd){
+  var cereal = {
+    name: n,
+    brand: b,
+    protein: p,
+    carbs: c,
+    fats: f,
+    sugar: s,
+    sodium: sd,
+    x: 0;
+    y: 0;
+  };
+  cereal.display = function(){
+    esveegee.appendChild(cereal.circleObj);
+  }
+  cereal.createCirc = function(){
+    var cereacle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    cereacle.setAttribute("fill", "yellow");
+    cereacle.setAttribute("r", 50);
+    cereacle.setAttribute("cx", cereal.x);
+    cereacle.setAttribute("cy", cereal.y);
+    return cereacle;
+  };
+  cereal.circleObj = createCirc();
+  cereal.hide = function(){
+    esveegee.removeChild();
+  };
+}
 
 var kell_click = function(){
   for(cereal in objs){
-    
+    if(cereal.brand = "Kellogs"){
+      cereal.x = Math.random() * 700;
+      cereal.y = Math.random() * 800;
+      cereal.display();
+    }
   }
 }
+
+
+var resize_protein = function(){
+  for(cereal in objs){
+    d3.select("#"+objs[cereal].name).data(objs[cereal].protein).attr("r", function(d){ d * 10 });
+
+  }
